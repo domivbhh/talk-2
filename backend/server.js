700d855e6e9e3524c674a26b9379d50c8f39b72c
domivbhh <hariharanvbit123@gmail.com>
2024-04-27 00:01:27 +0530

    starting

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const { chats } = require('./data/data.js');
+const dotenv=require('dotenv')
+
+const app=express()
+dotenv.config()
+
+const port=process.env.PORT||5000
+
+app.get('/',(req,res)=>{
+    res.send('Api is Running')
+
+});
+
+
+
+app.get('/api/chats',(req,res)=>{
+    res.send(chats)
+})
+
+app.get('/api/chats/:id',(req,res)=>{
+    // console.log(req.params.id)
+    const singleChat=chats.find((c)=>c._id===req.params.id)
+    res.send(singleChat)
+})
+
+
+app.listen(5000,console.log('server on port 5000'))
